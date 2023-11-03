4aa6b00e90867dfa3bb028a22f2b0c5d2c4dad12
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-11-03 14:44:16 +0900

    [level unrated] Title: a와 b 출력하기, Time: 19.20 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] input = Console.ReadLine().Split();
+        Console.WriteLine($"a = {input[0]}");
+        Console.WriteLine($"b = {input[1]}");
+    }
+}
