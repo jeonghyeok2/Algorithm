eea53dcd103952ae90fb00c26ae7fe91d8fa9167
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-11-05 23:42:08 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.16 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
@@ -1,16 +1,7 @@
 public class Solution {
     public string solution(string s) {
-        string answer = "";
-        int ints = 0;
-        if (s.Length % 2  == 0)
-        {
-            ints = (s.Length / 2) - 1;
-            return answer = s[ints].ToString() + s[ints + 1];
-        }
-        else
-        {
-            ints = s.Length / 2;
-            return answer = s[ints].ToString();
-        }
+        int n = (s.Length + 1) % 2; //홀수 = 0이 나와야 하므로 +1 해서 나머지 구하기
+        int l = (s.Length / 2) - n;
+        return s.Substring(l,n + 1);
     }
 }
