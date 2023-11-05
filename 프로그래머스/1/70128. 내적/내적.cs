7055dfa9999210743c1bf90055bdc514b0db8ce7
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-11-05 23:56:28 +0900

    [level 1] Title: 내적, Time: 1.59 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.cs
+++ 프로그래머스/1/70128. 내적/내적.cs
@@ -1,10 +1,9 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int solution(int[] a, int[] b) {
-        int answer = 0;
-        for (int i = 0; i < a.Length; i++)
-            answer += a[i] * b[i];
+        int answer = a.Zip(b, (z, d) => z * d).Sum();
         return answer;
     }
 }
