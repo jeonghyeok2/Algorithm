9be415e5d8c26d5f532b638a2bef266fdd30bd7f
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-11-10 14:50:50 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.66 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cs
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cs
@@ -1,21 +1,17 @@
-using System;
-using System.Collections.Generic;
-
 public class Solution {
     public int solution(string t, string p) {
         int answer = 0;
-        List<string> list = new List<string>();
-        for (int i = 0; i < t.Length - (p.Length - 1); i++)//p의 크기만큼 배열을 만들어야 하니 p의 크기에 -1
+        string str = "";
+        long num = 0;
+        
+        for(int i = 0; i < t.Length - p.Length + 1; i++)
         {
-            string test = "";
-            for (int j = 0; j < p.Length; j++)
-                test += t[i+j].ToString();//i + j를 이용해 i번부터 p의 크기만큼 번호 저장 
-            list.Add(test);
-        }
-        for (int i = 0; i < list.Count; i++)
-        {
-            if (long.Parse(list[i]) <= long.Parse(p))//리스트에 닮긴 내용을 조건과 일치하는지 확인 후 ++
+            num = long.Parse(t.Substring(i, p.Length));
+            if(num <= long.Parse(p))
+            {
                 answer++;
+            }
+            
         }
         return answer;
     }
