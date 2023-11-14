f4b281a14f178bd2693952b07c24e12d30a39af2
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-11-14 11:02:11 +0900

    [Bronze V] Title: ??!, Time: 60 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Backjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input.Length <= 50)
+            {
+                Console.WriteLine(input + "??!");
+            }            
+        }
+    }
+}
