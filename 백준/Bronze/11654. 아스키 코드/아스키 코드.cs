7410636a76ef58d6789dc49a8691d759fcb104ea
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-11-14 11:07:09 +0900

    [Bronze V] Title: 아스키 코드, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Text;
+
+namespace Backjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = Convert.ToChar(Console.ReadLine());
+            int asc = Convert.ToInt32(input);
+            Console.WriteLine(asc);
+        }
+    }
+}
