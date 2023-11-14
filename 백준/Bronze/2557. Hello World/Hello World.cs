65100c00a7071e1afa04e959fa4b120c30a97d5f
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-11-14 11:00:50 +0900

    [Bronze V] Title: Hello World, Time: 52 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
