9a3f87908371ec55995f59a4c772d57f039a742d
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-11-14 11:06:57 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 60 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Text;
+
+namespace Backjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(str.Length);
+        }
+    }
+}
