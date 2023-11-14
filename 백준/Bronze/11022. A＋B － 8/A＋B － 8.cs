c9d94aafff555209daffca2c1ebb9461f3fb5ec2
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-11-14 11:05:04 +0900

    [Bronze V] Title: A+B - 8, Time: 68 ms, Memory: 6096 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text;
+
+namespace Backjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int max = int.Parse(Console.ReadLine());
+            int[] a = new int[max];
+            int[] b = new int[max];
+
+            for (int i = 0; i < max; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                a[i] = int.Parse(input[0]);
+                b[i] = int.Parse(input[1]);
+                string c = a[i] + " + " + b[i] + " = ";
+                sb.Append("Case #" + (i + 1) + ": " + c + (a[i] + b[i]) + "\n");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
