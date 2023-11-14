ce554bca45b19851c7ba9307c2eb6061ba9d3696
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-11-14 11:02:56 +0900

    [Bronze V] Title: 고양이, Time: 56 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Backjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+        }
+    }
+}
