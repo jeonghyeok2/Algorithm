d2e956e783fdf9f0d21730cc6440b7b16ecc2fb0
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-11-23 13:48:02 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.31 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cs
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cs
@@ -1,16 +1,16 @@
 public class Solution {
     public int[] solution(int n, int m) {
-        int[] answer = new int[2];
-        int max = 0;
-        int min = 0;
-        for (int i = 1; i <= ((n > m) ? n : m); i++)
+        int max = 1;
+        int min = 1;
+        for (int i = 2; i < ((n > m) ? n : m); i++)
         {
             if (n % i == 0 && m % i == 0)
+            {
                 max = i;
+            }
         }
-        min = (n * m) / max; //최소공배수는 두 수를 곱하고 최대공약수로 나누면 된다.
-        answer[0] = max;
-        answer[1] = min;
+        min = (n * m) / max;
+        int[] answer = { max, min };
         return answer;
     }
 }
