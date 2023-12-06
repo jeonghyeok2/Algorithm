78e8c215c6479df1ed364f731c28fc93f72b1600
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-12-06 14:18:03 +0900

    [level unrated] Title: 합성수 찾기, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.cs
+++ 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.cs
@@ -3,16 +3,6 @@ using System;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            int count = 0;
-            for (int j = 1; j <= n; j++)
-            {
-                if (i % j == 0)
-                    count++;
-            }
-            answer += (count >= 3) ? 1 : 0;
-        }
         return answer;
     }
 }
