9c36e5215338dd0a9363d7922655b8dd2cbcc1cf
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-12-13 10:40:11 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 1.46 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cs
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cs
@@ -4,42 +4,8 @@ using System.Collections.Generic;
 
 public class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        List<int> _minusList = new List<int>();
-        List<int> _plusList = new List<int>();
-
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            if (numbers[i] <= 0)
-            {
-                _minusList.Add(numbers[i]);
-            }
-            else
-            {
-                _plusList.Add(numbers[i]);
-            }
-        }
-        _minusList.Sort();
-        _plusList.Sort();
-
-        if (_minusList.Count >= 2 && _plusList.Count >= 2)
-        {
-            _minusList[_minusList.Count - 1] *= _minusList[_minusList.Count - 2];
-            _plusList[_plusList.Count - 1] *= _plusList[_plusList.Count - 2];
-            answer = (_minusList[_minusList.Count - 1] >= _plusList[_plusList.Count - 1]) ? _minusList[_minusList.Count - 1] : _plusList[_plusList.Count - 1];
-        }
-        else if (_minusList.Count == 1 && _plusList.Count == 1)
-        {
-            answer = _minusList[_minusList.Count - 1] * _plusList[_plusList.Count - 1];
-        }
-        else if (_minusList.Count >= 2 && _plusList.Count == 0)
-        {
-            answer = _minusList[_minusList.Count - 1] * _minusList[_minusList.Count - 2];
-        }
-        else if (_plusList.Count >= 2 && _minusList.Count == 0)
-        {
-            answer = _plusList[_plusList.Count - 1] * _plusList[_plusList.Count - 2];
-        }
-        return answer ;
+       int maxLen = numbers.Length - 1;
+        Array.Sort(numbers);
+        return (int)MathF.Max(numbers[0] * numbers[1], numbers[maxLen] * numbers[maxLen - 1]);
     }
 }
