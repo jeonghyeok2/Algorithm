67c4b788cdd8029268dc3a54abaa50aa527f5c58
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2023-12-20 09:31:00 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 8.73 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int[] solution(string s) {
+        int[] answer = new int[2];
+        int index = s.Length;
+        for (int i = 0; i < index; i++)
+        {
+            if (s == "1")
+                break;
+            int maxIndex = s.Length;
+            s = s.Replace("0", string.Empty);
+            answer[0]++;
+            answer[1] += maxIndex - s.Length;
+            s = Convert.ToString(s.Length, 2);
+        }
+        return answer;
+    }
+}
