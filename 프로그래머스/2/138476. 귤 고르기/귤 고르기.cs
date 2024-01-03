24617d17de47c81c0d5c9886407cfb34072cc36c
jeonghyeok2 <146924502+jeonghyeok2@users.noreply.github.com>
2024-01-03 09:56:27 +0900

    [level 2] Title: 귤 고르기, Time: 18.59 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cs
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cs
@@ -3,26 +3,25 @@ using System.Linq;
 using System.Collections.Generic;
 
 public class Solution {
-    public int solution(int k, int[] tangerine)
-    {
-         int answer = 0;
-         int total = 0;
-
-         var fruitCounts = tangerine
-             .GroupBy(x => x) //같은 과일끼리 그룹화 하기
-             .Select(group => new { Fruit = group.Key, Count = group.Count() }) //새로운 컬렉션 생성 
-             .OrderByDescending(x => x.Count) //내림차순 정렬
-             .ToList();
-
-         foreach (var item in fruitCounts)
-         {
-             total += item.Count;
-             answer++;
-             if (total >= k)
-             {
-                 break;
-             }
-         }
-         return answer;
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Dictionary<int, int> size = new Dictionary<int, int>();
+        foreach(int t in tangerine)
+        {
+            if(!size.TryAdd(t, 1))
+            {
+                size[t]+=1;
+            }
+        }
+        List<int> count = new List<int>(size.Values);
+        count.Sort();
+        int kCount = 0;
+        int kIndex = count.Count-1;
+        while(k>kCount)
+        {
+            kCount+=count[kIndex--];
+            answer++;
+        }
+        return answer;
     }
 }
