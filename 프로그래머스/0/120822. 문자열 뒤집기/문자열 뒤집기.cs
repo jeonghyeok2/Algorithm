4e3e2f6ae6822386c38cfc09347940766ac47283
jeonghyeok2 <wjdgur4189@naver.com>
2024-01-23 09:58:07 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.25 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.cs
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.cs
@@ -1,7 +1,8 @@
 using System;
+using System.Text;
 
 public class Solution {
-    public string solution(string my_string)
+   public string solution(string my_string)
     {
         string answer = Swapping(my_string);
         return answer;
@@ -9,11 +10,11 @@ public class Solution {
     public string Swapping(string my_string)
     {
         int length = my_string.Length;
-        string str = "";
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = length - 1; i >= 0; i--)
         {
-            str += my_string[i];
+            stringBuilder.Append(my_string[i]);
         }
-        return str;
+        return stringBuilder.ToString();
     }
 }
